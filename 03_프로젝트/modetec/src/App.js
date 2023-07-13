7411b726c6d14f145d68f0018c64bd5754382bc8
황유현 <rhrnak0519@naver.com>
2023-07-13 17:30:09 +0900

    hyh

--- 03_프로젝트/modetec/src/App.js
+++ 03_프로젝트/modetec/src/App.js
@@ -1,18 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 import Resource from "./screens/Resource";
 import WebAccess from "./screens/WebAccess";
 import AnomalyDetection from "./screens/AnomalyDetection";
 import WebScanning from "./screens/WebScanning";
 import Report from "./screens/Report";
-import NavbarElements from "./component/NavBarElements"
 import Title from "./component/Title";
+import Sidebar1 from "./component/Sidebar1";
+import Sidebar2 from "./component/Sidebar2";
+
+
+const Center = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  `;
+
+  const Bar = styled.div`
+  top: 200px;
+  width: 17rem;
+  height: 100%;
+`;
 
 const App = ()=> {
   return (
     <div className='App'>
       <BrowserRouter>
       <Title />
-      <NavbarElements />
+      <Center>
+        <Bar>
+          <Sidebar1/>
+          <Sidebar2 />
+        </Bar>
         <Routes>
           <Route path="/" element={<Resource />}/>
           <Route path="/webaccess" element={<WebAccess />}/>
@@ -20,6 +39,7 @@ const App = ()=> {
           <Route path="/webscanning" element={<WebScanning />}/>
           <Route path="/report" element={<Report />}/>
         </Routes>
+        </Center>
       </BrowserRouter>
     </div>
   );
