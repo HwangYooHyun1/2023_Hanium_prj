3829c4d65a08bf6337c547f544a7b0e28f26b02f
황유현 <rhrnak0519@naver.com>
2023-07-21 13:44:58 +0900

    hyh

--- 03_프로젝트/yoohyun/src/screens/Resource.js
+++ 03_프로젝트/yoohyun/src/screens/Resource.js
@@ -9,7 +9,7 @@ const Resource = (props) => {
   return (
     <div>
       <Title><h5>리소스 모니터</h5></Title>
-        <iframe src="http://3.36.169.149:5601/app/dashboards#/view/50e3d320-2383-11ee-a0f2-110032b8cf20?embed=true&_g=(refreshInterval:(pause:!f,value:60000),time:(from:now-24h%2Fh,to:now))&_a=()&hide-filter-bar=true" height="100%" width="1600"></iframe>
+      <iframe src="http://3.36.169.149:5601/app/dashboards#/view/50e3d320-2383-11ee-a0f2-110032b8cf20?embed=true&_g=(refreshInterval:(pause:!f,value:60000),time:(from:now-24h%2Fh,to:now))&_a=()&hide-filter-bar=true" height="100%" width="1600"></iframe>
     </div>
   )
 }
--- 03_프로젝트/yoohyun/src/screens/WebAccess.js
+++ 03_프로젝트/yoohyun/src/screens/WebAccess.js
@@ -9,7 +9,7 @@ const WebAccess = (props) => {
   return (
     <div>
       <Title><h5>웹 액세스 모니터</h5></Title>
-      <iframe src="http://3.36.169.149:5601/app/dashboards#/view/5bc2aa60-2201-11ee-a0f2-110032b8cf20?embed=true&_g=(refreshInterval:(pause:!f,value:60000),time:(from:now-7d%2Fd,to:now))&_a=()&hide-filter-bar=True" height="100%" width="1600"></iframe>
+      <iframe src="http://3.36.169.149:5601/app/dashboards#/view/5bc2aa60-2201-11ee-a0f2-110032b8cf20?embed=true&_g=(refreshInterval:(pause:!f,value:60000),time:(from:now-24h%2Fh,to:now))&_a=()&hide-filter-bar=True" height="100%" width="1600"></iframe>
     </div>
         
   )
