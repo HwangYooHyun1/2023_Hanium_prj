b9137e44662047af799b69463ea9caa6f37767ee
황유현 <rhrnak0519@naver.com>
2023-07-24 10:35:53 +0900

    hyh

--- 03_프로젝트/yoohyun/src/screens/AnomalyDetection.js
+++ 03_프로젝트/yoohyun/src/screens/AnomalyDetection.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import axios from 'axios';
+import ESResult from './ElasticSearchResult.json'
+
 
 const AnomalyDetection = () => {
   return (
     <div>
         <h4>AnomalyDetection</h4>
+        console.log()
     </div>
   )
 }
