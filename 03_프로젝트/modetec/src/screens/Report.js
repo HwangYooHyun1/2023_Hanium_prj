96faf19a8ce3e8aa9c3efc2d7c5b7e18da2566d8
황유현 <rhrnak0519@naver.com>
2023-08-07 17:42:48 +0900

    hyh

--- 03_프로젝트/modetec/src/screens/Report.js
+++ 03_프로젝트/modetec/src/screens/Report.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import Styled from 'styled-components'
-
-const Container = Styled.div`
-  height: 100vh; 
-  width: 100vw;
-  padding-left:30px;
-`;
-
-const Title = Styled.div`
-  padding-top: 60px;
-  padding-left: 10px;
-`;
 
 const Report = () => {
   return (
-    <Container>
-        <Title>Report</Title>
-    </Container>
+    <div>
+        <h4>Report</h4>
+    </div>
   )
 }
 
