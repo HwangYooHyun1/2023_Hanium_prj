0e7e7f153faccc94d19bcee708a10b55570c276f
황유현 <rhrnak0519@naver.com>
2023-08-07 17:53:34 +0900

    hyh

--- 03_프로젝트/modetec/src/screens/AnomalyDetection/AccessList.js
+++ 03_프로젝트/modetec/src/screens/AnomalyDetection/AccessList.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import MetricItem from './AccessItem';
 
-export const AccessList = () => {
+const AccessList = () => {
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://52.79.201.187:8080/loganomalies"
+        )
+        console.log("API response:", response.data);
+        setResult(response.data.info.logResponseData);
+      } catch (e) {
+        console.log("Error fetching data:", e);
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  console.log("Result:", result);
+  console.log("Loading:", loading);
+
+  if (loading) {
+    return <div><a>대기중...</a></div>
+  }
+  if (!result || result.length === 0) {
+    return null;
+  }
+
   return (
-    <div>AccessList</div>
-  )
+    <div className='results'>
+      {result.map((result) => (
+        <MetricItem result={result} />
+      ))}
+    </div>
+  );
 }
+
 
 export default AccessList;
