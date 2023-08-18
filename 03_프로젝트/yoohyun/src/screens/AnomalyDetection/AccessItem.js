0b28ee6ff3e7cb856e3ef6051bc2989ed4405c9e
황유현 <rhrnak0519@naver.com>
2023-08-18 12:41:42 +0900

    hyh

--- 03_프로젝트/yoohyun/src/screens/AnomalyDetection/AccessItem.js
+++ 03_프로젝트/yoohyun/src/screens/AnomalyDetection/AccessItem.js
@@ -6,51 +6,34 @@ const TableRow = styled.tr`
     padding: 10px;
     border: 1px inset #ccc;
     font-size: 1.1rem;
-    border-left: none; /* 왼쪽 선 제거 */
-    border-right: none; /* 오른쪽 선 제거 */
-  
+    border-left: none;
+    border-right: none;
+  }
 `;
+
 const ScoreBox = styled.div`
   width: 20px;
   height: 20px;
   margin: 0 auto;
   border: 0.5px solid #000;
-  background-color: ${props => getScoreColor(props.score)};
+  background-color: ${(props) => getScoreColor(props.score)};
 `;
-
 
 const getScoreColor = (score) => {
   if (score >= 25 && score < 50) {
     return "#fddd00";
   } else if (score >= 50 && score < 75) {
     return "#fba740";
-  } else if (score >= 57 && score <= 100) {
+  } else if (score >= 75 && score <= 100) {
     return "#fe5050";
   } else {
     return "#8bc8fb";
   }
-};;
-
-// 시간을 한국 표준 시간대로 변환하는 함수
-const convertToKST = (timestamp) => {
-  const date = new Date(timestamp);
-  const kstOffset = 9 * 60; // KST는 UTC+9
-  date.setMinutes(date.getMinutes() + kstOffset);
-
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 };
 
-const MetricItem = ({ result }) => {
+const AccessItem = ({ result }) => {
   const { detector, time, score, sourceIp } = result;
-  const kstTime = convertToKST(time); // 시간 변환
-
+  const kstTime = new Date(time).toLocaleString(); 
   const formattedScore = score.toFixed(2);
   const scoreColor = getScoreColor(score);
 
@@ -60,9 +43,11 @@ const MetricItem = ({ result }) => {
       <td>{detector}</td>
       <td>{sourceIp !== null ? sourceIp : ""}</td>
       <td>{formattedScore}</td>
-      <td><ScoreBox score={score} style={{ backgroundColor: scoreColor }} /></td>
+      <td>
+        <ScoreBox score={score} style={{ backgroundColor: scoreColor }} />
+      </td>
     </TableRow>
   );
 };
 
-export default MetricItem;
+export default AccessItem;
--- 03_프로젝트/yoohyun/src/screens/ResourceInfo/ResourceInfo.js
+++ 03_프로젝트/yoohyun/src/screens/ResourceInfo/ResourceInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Styled from 'styled-components';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -17,7 +17,7 @@ const Sidebar = Styled.div`
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
   overflow-y: auto;
   position: fixed;
-  left: 270px;
+  left: 260px;
   top: 90px;
   bottom: 0;
 `;
@@ -33,19 +33,38 @@ const ContentWrapper = Styled.div`
 export const ResourceInfo = (props) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    // 아이템 목록을 가져오는 비동기 로직을 작성하고 itemList 상태 업데이트
+    fetch('http://52.79.201.187:8080/agents')
+      .then(response => response.json())
+      .then(data => {
+        const infoMap = data.info.map;
+        const newItems = Object.keys(infoMap).map(key => {
+          const url = `http://3.36.169.149:5601/app/dashboards#/view/aa4a4aa0-379e-11ee-9fc5-9ddfb64e9cde?embed=true&_g=(time:(from:now-24h%2Fh,to:now))&_a=(filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'metricbeat-*',key:host.hostname,negate:!f,params:(query:${infoMap[key]}),type:phrase),query:(match_phrase:(host.hostname:${infoMap[key]})))))&hide-filter-bar=true`;
+          return {
+            name: key,
+            nameValue: infoMap[key],
+            url: url,
+          };
+        });
+        setItemList(newItems);
+      })
+      .catch(error => console.error('Error fetching items:', error));
+  }, []);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   const renderIframe = () => {
-    if (selectedItem === 'Item 1') {
-      return <iframe src="http://3.36.169.149:5601/app/dashboards#/view/aa4a4aa0-379e-11ee-9fc5-9ddfb64e9cde?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-24h%2Fh%2Cto%3Anow))&hide-filter-bar=true" height="100%" width="100%"></iframe>
-    } else if (selectedItem === 'Item 2') {
-      return <iframe src="http://3.36.169.149:5601/app/dashboards#/view/aa4a4aa0-379e-11ee-9fc5-9ddfb64e9cde?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-24h%2Fh%2Cto%3Anow))&hide-filter-bar=true" height="100%" width="100%"></iframe>
+    if (selectedItem) {
+      return <iframe src={selectedItem.url} height="100%" width="100%"></iframe>;
     }
     return null;
   };
+
 
   return (
     <div className='rContainer'>
@@ -59,8 +78,11 @@ export const ResourceInfo = (props) => {
               <FontAwesomeIcon icon={faAngleLeft} />
             </div>
             <h5 style={{ padding: '15px' }}>Server List</h5>
-            <div className='AccordionButton' onClick={() => setSelectedItem('Item 1')}>Item 1</div>
-            <div className='AccordionButton' onClick={() => setSelectedItem('Item 2')}>Item 2</div>
+            {itemList.map(item => (
+              <div className='AccordionButton' key={item.name} onClick={() => setSelectedItem(item)}>
+                {item.name} <br /> ({item.nameValue})
+              </div>
+            ))}
           </Sidebar>
         )}
         {!isSidebarOpen && (
