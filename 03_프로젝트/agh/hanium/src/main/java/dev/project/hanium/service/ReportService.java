cd7279ae8ffb5d065955012293bfa3e4d83c7c51
Ahngyuho <gyuho0897@gmail.com>
2023-09-03 22:37:40 +0900

    agh - report api에 metrtic 정보 추가

--- 03_프로젝트/agh/hanium/src/main/java/dev/project/hanium/service/ReportService.java
+++ 03_프로젝트/agh/hanium/src/main/java/dev/project/hanium/service/ReportService.java
@@ -1,6 +1,7 @@
 package dev.project.hanium.service;
 
 import dev.project.hanium.Entity.RequestDate;
+import dev.project.hanium.dto.MetricResultAnomalyDto;
 import dev.project.hanium.repository.LogAnomalyRepository;
 import dev.project.hanium.repository.MetricAnomalyRepository;
 import dev.project.hanium.dto.ReportDto;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -19,6 +21,7 @@ import static java.util.stream.Collectors.*;
 public class ReportService {
     private final MetricAnomalyRepository metricAnomalyRepository;
     private final LogAnomalyRepository logAnomalyRepository;
+    private final MetricsService metricsService;
 
     public List<ReportDto> reports(LocalDateTime startDate, LocalDateTime endDate) {
         List<ReportDto> metric = metricAnomalyRepository.findMetricAnomalyByTimeBetween(startDate, endDate)
@@ -26,8 +29,13 @@ public class ReportService {
 
         List<ReportDto> log = logAnomalyRepository.findLogAnomaliesByTimeBetween(startDate, endDate)
                 .stream().map(ReportDto::fromLog).collect(toList());
-
-        return Stream.concat(metric.stream(), log.stream())
+        List<ReportDto> collect = Stream.concat(metric.stream(), log.stream()).sorted(Comparator.comparing(ReportDto::getTime).reversed())
                 .collect(toList());
+        for (int i = 0; i < collect.size(); i++) {
+            ReportDto report = collect.get(i);
+            MetricResultAnomalyDto metricInfo = metricsService.getMetricsForDate(report.getTime());
+            collect.get(i).setMetricInfo(metricInfo);
+        }
+        return collect;
     }
 }
