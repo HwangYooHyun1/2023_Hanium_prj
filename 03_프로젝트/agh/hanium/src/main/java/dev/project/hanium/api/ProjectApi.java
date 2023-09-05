95f6a9ecc76fa4b81fbde784445d54ff714ff351
Ahngyuho <gyuho0897@gmail.com>
2023-09-05 10:06:14 +0900

    agh - agent 생성 시 project 리스트 불러오는 api 작성

--- 03_프로젝트/agh/hanium/src/main/java/dev/project/hanium/api/ProjectApi.java
+++ 03_프로젝트/agh/hanium/src/main/java/dev/project/hanium/api/ProjectApi.java
@@ -5,9 +5,12 @@ import dev.project.hanium.request.group.CreateProjectRequest;
 import dev.project.hanium.response.CommonResponse;
 import dev.project.hanium.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +20,10 @@ public class ProjectApi {
     public CommonResponse<Void> createGroup(@RequestBody CreateProjectRequest createProjectRequest) {
         projectService.create(ProjectDto.fromCreate(createProjectRequest));
         return CommonResponse.success();
+    }
+
+    @GetMapping("/projects")
+    public CommonResponse<List<String>> getProjectsName() {
+        return CommonResponse.success(projectService.getAgents());
     }
 }
